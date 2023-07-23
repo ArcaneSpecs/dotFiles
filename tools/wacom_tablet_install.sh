a69b6635d20a7c0ae5dd7fa29c2776f2993f777a
Patrik Jylhä <patemark997@gmail.com>
2023-07-23 19:21:38 +0300

    Wacom artist mode fix

--- tools/wacom_tablet_install.sh
+++ tools/wacom_tablet_install.sh
@@ -6,6 +6,22 @@ pushd "$(dirname "$0")/" > /dev/null
 # echo "blacklist wacom" | sudo tee -a /etc/modprobe.d/blacklist.conf
 # sudo rmmod wacom
 
+# https://opentabletdriver.net/Wiki/FAQ/Linux#artist-mode
+# Run these to fix artist mode
+
+# 1. Create an override file 
+# Opens with whatever program is set to $EDITOR
+# systemctl --user edit opentabletdriver.service
+
+# 2. Add this block below the first 2 lines of the file, then save and exit 
+# [Service]
+# Environment=WAYLAND_DISPLAY=:0
+# 3. Add this block below the first 2 lines of the file, then save and exit 
+# Reload the systemd user daemon
+# systemctl --user daemon-reload
+# Start the OpenTabletDriver daemon service (restarts if already running)
+# systemctl --user restart opentabletdriver.service
+
 yay -S opentabletdriver
 systemctl --user daemon-reload
 systemctl --user enable opentabletdriver --now
