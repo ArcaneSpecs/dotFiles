6acf40526e50bd47c0257069fd3da37ece46934b
Patrik Jylhä <patemark997@gmail.com>
2025-08-31 13:05:26 +0300

    Comfyui script update

--- tools/comfyui/install.sh
+++ tools/comfyui/install.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+
+# NOTE: For libstdc++.so.6 error:
+# cd /home/my_user/.conda/envs/comfyenv/bin/../lib/
+# NOTE: backup the old libstdc++.so.6 if something goes wrong
+# mv libstdc++.so.6 libstdc++.so.6_old
+# ln -s /usr/lib/libstdc++.so.6 libstdc++.so.6
+
 # NOTE: Run in conda envs and comfyui root dir if we get error loading this ↓
 # find . \( -name "libhiprtc.so" -o -name "libamdhip64.so" \) -exec patchelf --clear-execstack {} \;
 
-rm -rf venv
-python -m venv venv
+# rm -rf venv
+# python -m venv venv
+
+conda create -n comfyenv python=3.12
+conda activate comfyenv
+
 if [ $? -eq 0 ]; then
-    source venv/bin/activate
+    # source venv/bin/activate
     pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm6.2.4
     pip install -r requirements.txt
 fi
--- tools/comfyui/run.sh
+++ tools/comfyui/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=/home/patu/github/ComfyUI/venv/lib/python3.13/site-packages/pymeshlab/lib/:$LD_LIBRARY_PATH
-source venv/bin/activate
 
-HSA_OVERRIDE_GFX_VERSION=11.0.0 python main.py --use-pytorch-cross-attention
+export LD_LIBRARY_PATH=/home/patu/.conda/envs/comfyenv/lib/python3.12/site-packages/pymeshlab/lib:$LD_LIBRARY_PATH
+source ~/.conda_init
+conda activate comfyenv
 
+# HSA_OVERRIDE_GFX_VERSION=11.0.0 python main.py --use-pytorch-cross-attention
+python main.py
